227570614d54f304542f9d04931f789f5689f3bf
Anunciada <jamilly.d.castro@accenture.com>
2023-02-09 14:13:22 -0300

    first commit: project structcue create

---
+++ src/main/java/com/darllen/sizani/SizaniApplication.java
@@ -0,0 +1,13 @@
+package com.darllen.sizani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SizaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SizaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darllen/sizani/SizaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darllen.sizani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SizaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
