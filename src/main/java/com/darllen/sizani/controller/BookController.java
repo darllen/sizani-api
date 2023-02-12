f9009457e1e0380d8ddc14994e8ce0aefd3b8a02
Anunciada <jamilly.d.castro@accenture.com>
2023-02-11 23:11:36 -0300

    includes getByID and getAll functions

--- src/main/java/com/darllen/sizani/controller/BookController.java
+++ src/main/java/com/darllen/sizani/controller/BookController.java
@@ -1,15 +1,11 @@
 package com.darllen.sizani.controller;
 
 import com.darllen.sizani.dto.BookDTO;
-import com.darllen.sizani.dto.ResponseDTO;
 import com.darllen.sizani.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
@@ -22,8 +18,33 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @PostMapping()
-    public ResponseDTO create(@RequestBody @Valid BookDTO bookDTO){
-        return bookService.create(bookDTO);
+//    @PostMapping()
+//    public ResponseDTO create(@RequestBody @Valid BookDTO bookDTO){
+//        return bookService.create(bookDTO);
+//    }
+
+    @PostMapping
+    @ResponseBody
+    private String create(@RequestBody BookDTO book){
+        BookDTO savedBook = bookService.create(book);
+        return "Book created with ID: " + savedBook.getId();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public BookDTO getByID(@PathVariable Long id){
+        return bookService.getByID(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public String delete(@PathVariable("id") Long id){
+        return bookService.delete(id);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<BookDTO> getAll(){
+        return bookService.getAll();
     }
 }
--- src/main/java/com/darllen/sizani/service/BookService.java
+++ src/main/java/com/darllen/sizani/service/BookService.java
@@ -7,25 +7,49 @@ import com.darllen.sizani.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
 public class BookService {
     public BookRepository bookRepository;
+    private static final Map<Long, BookDTO> books = new HashMap<>();
+
 
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    public ResponseDTO create(BookDTO bookDTO){
+//    public ResponseDTO create(BookDTO bookDTO){
+//
+//        Book bookToSave = Book.builder()
+//                .title(bookDTO.getTitle())
+//                .isbn(bookDTO.getIsbn())
+//                .author(bookDTO.getAuthor())
+//                .publisher(bookDTO.getPublisher())
+//                .build();
+//
+//        Book savedBook = bookRepository.save(bookToSave);
+//        return ResponseDTO.builder().message("Book created with ID: " + savedBook.getId()).build();
+//    }
 
-        Book bookToSave = Book.builder()
-                .title(bookDTO.getTitle())
-                .isbn(bookDTO.getIsbn())
-                .author(bookDTO.getAuthor())
-                .publisher(bookDTO.getPublisher())
-                .build();
+    public BookDTO create(BookDTO book){
+        Long id = books.keySet().size() + 1L;
+        book.setId(id);
 
-        Book savedBook = bookRepository.save(bookToSave);
-        return ResponseDTO.builder().message("Book created with ID: " + savedBook.getId()).build();
+        books.put(id, book);
+        return book;
+    }
+    public BookDTO getByID(Long id){
+        return books.get(id);
+    }
+
+    public List<BookDTO> getAll(){
+        return new ArrayList<>(books.values());
+    }
+
+    public String delete(Long id){
+        books.remove(id);
+        return "deleted!";
     }
 }
